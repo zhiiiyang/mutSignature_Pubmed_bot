e45eee1c71bf8f42210511df486c1be9e97ddd0d
Zhi Yang <zhiyang@usc.edu>
2023-06-02 22:29:21 -0700

    fix pubmed (char needs to be less than 140)

--- lambda_function.py
+++ lambda_function.py
@@ -22,23 +22,21 @@ def retweet_pubmed_bot():
     if 'RT @' in status.text:
         id_strs.append(status.retweeted_status.id)
   search_terms = ['#MutationalSignatures','#MutationalSignature', 'mutational signatures', 'mutational signature']
-  yesterday = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days = 1)
-  
+
   for search_term in search_terms:
-    tweets=api.search_tweets(search_term, count=10, lang="en")
+    tweets=api.search_tweets(search_term, count=5, lang="en")
     for tweet in tweets:
       if tweet.id not in id_strs and tweet.author.name != "mutational signatures twitbot" and 'RT @' not in tweet.text:
         api.retweet(tweet.id)
               
   #pubmed twitter            
   pubmed = PubMed(tool="MyApp", email="zhiyang@usc.edu")
-  results = pubmed.query('\"mutational signature\" AND (("2020/01/01"[Date - Create] : "3000"[Date - Create]))', max_results=1000)
+  results = pubmed.query('\"mutational signature\" AND (("2020/01/01"[Date - Create] : "3000"[Date - Create]))', max_results=5)
   # Loop over the retrieved articles
   keyword = "mutational signature"
   tweets_text = [i.text for i in timeline] 
   for article in results:
-    if all(article.title[0:10] not in substring for substring in tweets_text):
-        #print(article.title + " " + " https://doi.org/" + article.doi)
-        #print(article.doi)
-        api.update_status(article.title + " " + " https://doi.org/" + article.doi)
+    if all(article.title not in substring for substring in tweets_text):
+        #print(result.title + + "https://doi.org/" + result.doi)
+        print((article.title[:(140-4-len("https://doi.org/" + article.doi))] + "... https://doi.org/" + article.doi))
         break
